1cf6c0c0063fcb06168adc23f0ad2e9799494000
Vyacheslav <myeve2007@yandex.ru>
2019-11-08 20:48:07 +0300

    feat: add canvas init and canvas grid

--- palette/script.js
+++ palette/script.js
@@ -1,3 +1,21 @@
 window.onload = function () {
+    const canvas = document.querySelector('.work-area-canvas-block__canvas'),
+          ctx = canvas.getContext('2d'),
+          canvasFrameSize = 512,
+          columns = [],
+          rows = [];
+    let pixelSize = 4,
+        colorToFillTemplate = '#41f795';
 
+    for (let i = 0; i < canvasFrameSize / pixelSize; i++) {
+        columns.push(i);
+        rows.push(i);
+    }
+
+    columns.forEach((columnValue, i) => {
+        rows.forEach((rowValue, j) => {
+            ctx.fillStyle = colorToFillTemplate;
+            ctx.fillRect(i*pixelSize, j*pixelSize, pixelSize, pixelSize);
+        });
+    })
 };
